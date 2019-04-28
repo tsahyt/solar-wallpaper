ff9193dcd4a5808d11e96183c47d1aa929659ef9
Paul Ogris <paul@tsahyt.com>
2019-04-28 08:50:51 +0200

    Add wallpaper effect

--- src/Polysemy/Wallpaper.hs
+++ src/Polysemy/Wallpaper.hs
@@ -1,20 +1,36 @@
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Polysemy.Wallpaper
     ( Wallpaper(..)
     , setWallpaper
+    , runWallpaperIO
     ) where
 
 import Polysemy
 
--- import qualified GI.Gio as Gio
+import Control.Monad
+import Data.Text (pack)
+import qualified GI.Gio as Gio
 
 data Wallpaper m a where
     SetWallpaper :: FilePath -> Wallpaper m ()
 
 makeSem ''Wallpaper
 
+makeSettings :: IO Gio.Settings
+makeSettings = Gio.settingsNew "org.gnome.desktop.background"
+
 runWallpaperIO :: Member (Lift IO) r => Sem (Wallpaper ': r) a -> Sem r a
-runWallpaperIO = do
-  --  gsettings <- Gio.settingsNew "org.gnome.desktop.background"
-    undefined
+runWallpaperIO sem = do
+    gsettings <- sendM makeSettings
+    interpret
+        (\case
+             SetWallpaper path ->
+                 sendM @IO $
+                 void $
+                 Gio.settingsSetString
+                     gsettings
+                     "picture-uri"
+                     ("file://" <> pack path))
+        sem
